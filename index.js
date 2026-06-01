545e95d53066b2cdcc788d283fedaa3d2f050f97
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-01 19:28:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on", PORT);
+  console.log("Server running at", PORT);
 });
