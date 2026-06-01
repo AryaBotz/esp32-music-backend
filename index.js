959206d8e753fe88faa2503714aac78d9fe562c3
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-01 18:17:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const cors = require("cors");
 
 const chatRoute = require("./routes/chat");
 const musicRoute = require("./routes/music");
+const sttRoute = require("./routes/stt");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/chat", chatRoute);
 app.use("/music", musicRoute);
+app.use("/stt", sttRoute);
 
-// health check
 app.get("/", (req, res) => {
-  res.json({
-    status: "backend running clean architecture"
-  });
+  res.json({ status: "ESP32 AI backend running" });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on", PORT);
 });
